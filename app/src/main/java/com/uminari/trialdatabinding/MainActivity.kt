975deef1748c1ec1bbc7b7a3be9aa81777b01e9f
RyujiKoyama95 <ryujikoyama95@gmail.com>
2023-01-24 09:08:53 +0900

    Add preparation for data binding

--- app/src/main/java/com/uminari/trialdatabinding/MainActivity.kt
+++ app/src/main/java/com/uminari/trialdatabinding/MainActivity.kt
@@ -2,10 +2,15 @@ package com.uminari.trialdatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uminari.trialdatabinding.views.InputTextFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, InputTextFragment())
+        fragmentTransaction.commit()
     }
 }
